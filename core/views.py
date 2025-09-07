78f6b89b1351ef2f79d5a64660e559dfb1dcb658
muradaghazade <xomurad41@gmail.com>
2025-09-07 15:56:36 +0400

    m

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from core.models import *
 
 def home(request):
-    return render(request, 'index.html')
+    blog = Blog.objects.order_by("-id")
+    partners = Partner.objects.order_by("-id")
+    context = {
+        'blog': blog,
+        'partners': partners,
+    }
+    return render(request, 'index.html', context)
