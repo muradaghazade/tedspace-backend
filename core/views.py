568bba2a84d563f33a1a96f4a54e2e5bf26087fa
muradaghazade <xomurad41@gmail.com>
2025-09-14 15:38:39 +0400

    m

--- core/views.py
+++ core/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import *
+from core.forms import ContactForm
+from django.core.mail import send_mail
 
 def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Send email notification
+            name = form.cleaned_data['name']
+            surname = form.cleaned_data['surname']
+            email = form.cleaned_data['email']
+            send_mail(
+                'New Contact Form Submission',
+                f'You have a new contact form submission from {name} {surname}, email: {email}.',
+                'murad.aghazada@div.edu.az',
+                ['murad.aghazada@div.edu.az'],
+                fail_silently=False,
+            )
+            send_mail(
+                'Tedspace Contact Form Submission Successful!',
+                f'Dear {name},\n\nThank you for reaching out to Tedspace. We have received your message and will get back to you shortly.\n\nBest regards,\nTedspace Team',
+                'murad.aghazada@div.edu.az',
+                [email],
+                fail_silently=False,
+            )
+            return redirect('/')
+    form = ContactForm()
     blog = Blog.objects.order_by("-id")
     partners = Partner.objects.order_by("-id")
     context = {
         'blog': blog,
         'partners': partners,
+        'form': form,
     }
     return render(request, 'index.html', context)
--- tedspace/settings.py
+++ tedspace/settings.py
@@ -126,3 +126,13 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+EMAIL_HOST_USER = "murad.aghazada@div.edu.az"
+EMAIL_HOST_PASSWORD = "xyay mgrm duyf yfnz"  # Google App Password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
